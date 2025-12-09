bbc8fdc25be2a5f466508b8a87e1dd383156c756
Praise Oguntibeju <40713306@live.napier.ac.uk>
2025-12-09 11:46:20 +0000

    header, footer and navigation.

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask,url_for, render_template
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    image_url = url_for('static', filename='img/logo.png')
-    return render_template("index.html", image=image_url)
+    return render_template("index.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/countries/")
+def countries():
+    return render_template("countries.html")
